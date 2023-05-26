5c184569a7cff5b1dada895c12a082acbcf9f156
Joao Martinho <jdmartinho@gmail.com>
2023-05-26 19:38:36 +0100

    add table with data when clicking on council

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+// Function to get the nth key from the object
+Object.prototype.getByIndex = function(index) {
+  return this[Object.keys(this)[index]];
+};
+
 // Styles to apply
 // Default style
 var style = {
@@ -80,17 +85,59 @@ for (var concelhoName in concelhosSVG) {
     }, true);
 
     concelho[0].addEventListener("click", function() {
-      //concelho.animate(corConcelho(nome), animationSpeed);
-      console.log('clicked the mouse');
-      console.log(nome);
       var selectedCouncil = councilData.find(council => {
         return council.municipio == nome;
       });
-      console.log(selectedCouncil.residentes2021);      
-      document.getElementById('council').style.display = 'block';
-      //document.getElementById('council').innerHTML = 'Hello';
-      document.getElementById('council').innerHTML = selectedCouncil.residentes2021;
+      fillInformation(selectedCouncil);
     }, true);
 
   })(concelhosSVG[concelhoName], concelhoName);
+}
+
+function fillInformation(council) { 
+  var keys = Object.keys(council);
+  document.getElementById('councilName').innerHTML = council['municipio'];
+
+  // Draw table with headers
+  var tableToFill = document.getElementById('results');
+  tableToFill.innerHTML = '';
+  var tblHeader = document.createElement('thead');
+  var header2011 = document.createElement('th');
+  header2011.innerHTML = '2011';
+  header2011.scope = 'col';
+  var header2021 = document.createElement('th');
+  header2021.innerHTML = '2021';
+  header2021.scope = 'col';
+  tblHeader.appendChild(header2011);
+  tblHeader.appendChild(header2021);
+  tableToFill.appendChild(tblHeader);
+  
+  // Fill in rows, 2 elements at a time because they are a "pair"
+  // of related values, for 2011 and 2021
+  var tblBody = document.createElement('tbody');
+  var x = 2;
+  while(x < keys.length) {
+    var headerRow = document.createElement('tr');
+    var headerText2011 = document.createElement('td')
+    headerText2011.innerHTML = keys[x];
+    var headerText2021 = document.createElement('td')
+    headerText2021.innerHTML = keys[x+1];
+    headerRow.appendChild(headerText2011);
+    headerRow.appendChild(headerText2021);
+
+    var valuesRow = document.createElement('tr');
+    var valuesText2011 = document.createElement('td')
+    valuesText2011.innerHTML = council.getByIndex(x);
+    var valuesText2021 = document.createElement('td')
+    valuesText2021.innerHTML = council.getByIndex(x+1);
+    valuesRow.appendChild(valuesText2011);
+    valuesRow.appendChild(valuesText2021);
+
+    tblBody.appendChild(headerRow);
+    tblBody.appendChild(valuesRow);
+
+    tableToFill.appendChild(tblBody);
+
+    x=x+2;
+  }
 }
